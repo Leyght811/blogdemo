d3ec2eccef899f991456d496196f7bb82f8c9593
Leyght811 <wesleyhamish@hotmail.com>
2025-09-11 13:54:41 +1200

    delete post

--- theblog/forms.py
+++ theblog/forms.py
@@ -8,7 +8,7 @@ class PostForm(forms.ModelForm):
 
         widgets = {
             'title': forms.TextInput(attrs={'class':'form-control'}),
-            'author': forms.TextInput(attrs={'class':'form-control'}),
+            'author': forms.Select(attrs={'class':'form-control'}),
             'body': forms.Textarea(attrs={'class':'form-control'}),
         }
 
--- theblog/urls.py
+++ theblog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article_details'),
     path('add_post/', views.AddPostView.as_view(), name='add_post'),
     path('article/edit/<int:pk>', views.UpdatePostView.as_view(), name='edit_post'),
+    path('article/delete/<int:pk>', views.DeletePostView.as_view(), name='delete_post'),
 ]
